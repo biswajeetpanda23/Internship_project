e6f0c50392d56eb726514c952ce762da6e23e1cb
biswajeet panda <pandabiswajeet5@gmail.com>
2025-02-22 11:54:16 +0530

    1st commit

--- assets/js/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const mobileMenu = document.querySelector(".mobile-menu");
-    const navList = document.querySelector("nav ul");
-
-    mobileMenu.addEventListener("click", function () {
-        navList.classList.toggle("show");
-    });
-});
